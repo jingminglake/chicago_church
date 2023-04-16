2f08fc39eca545fc46d6f42e4857a6b45f3f055b
Yingson Yu <yingsonyu@gmail.com>
2023-04-15 21:01:47 -0500

    nav and home top

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,10 +3,18 @@ import React from "react";
 import "./home.scss";
 
 const Home = () => {
-  console.log("hi");
   return (
     <div>
-      <div>hi</div>
+      <div
+        style={{ backgroundImage: `url(${"/assets/home/top.jpeg"})` }}
+        className='home-top'
+      >
+        <div className='home-qw'>
+          <div className='home-q'>KNOWING CHRIST</div>
+
+          <div className='home-w'>and making Him known</div>
+        </div>
+      </div>
     </div>
   );
 };
