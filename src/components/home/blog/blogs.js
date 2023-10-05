129580940e3890d5095925d336f2f52a7b42d317
Yaxiong Hu <1006738657@qq.com>
2023-10-05 01:24:46 -0500

    post music blog

--- src/components/home/blog/blogs.js
+++ src/components/home/blog/blogs.js
@@ -55,20 +55,23 @@ const blogs = [
 
   {
     id: 4,
-    title: "Pumpkin Patch Meetup @7",
-    body: `RANDOM TEXT
-
-      Material confined likewise it humanity raillery an unpacked as he. Three chief merit no if. Now how her edward engage not horses. Oh resolution he dissimilar precaution to comparison an. Matters engaged between he of pursuit manners we moments. Merit gay end sight front. Manor equal it on again ye folly by match. In so melancholy as an sentiments simplicity connection. Far supply depart branch agreed old get our.
-
-  Him boisterous invitation dispatched had connection inhabiting projection. By mutual an mr danger garret edward an. Diverted as strictly exertion addition no disposal by stanhill. This call wife do so sigh no gate felt. You and abode spite order get. Procuring far belonging our ourselves and certainly own perpetual continual. It elsewhere of sometimes or my certainty. Lain no as five or at high. Everything travelling set how law literature.
-
-  Folly was these three and songs arose whose. Of in vicinity contempt together in possible branched. Assured company hastily looking garrets in oh. Most have love my gone to this so. Discovered interested prosperous the our affronting insipidity day. Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible.
-
-  Much did had call new drew that kept. Limits expect wonder law she. Now has you views woman noisy match money rooms. To up remark it eldest length oh passed. Off because yet mistake feeling has men. Consulted disposing to moonlight ye extremity. Engage piqued in on coming.
+    title: "Music Literacy Class",
+    body: `
+        16:14 耶和华的灵离开扫罗，有恶魔从耶和华那里来扰乱他。
+        15 扫罗的臣仆对他说，现在有恶魔从神那里来扰乱你。
+        16 我们的主可以吩咐面前的臣仆，找一个善于弹琴的来，等神那里来的恶魔临到你身上的时候，使他用手弹琴，你就好了。
+        17 扫罗对臣仆说，你们可以为我找一个善于弹琴的，带到我这里来。
+        18 其中有一个少年人说，我曾见伯利恒人耶西的一个儿子善于弹琴，是大有勇敢的战士，说话合宜，容貌俊美，耶和华也与他同在。
+        19 于是扫罗差遣使者去见耶西，说，请你打发你放羊的儿子大卫到我这里来。
+        20 耶西就把几个饼和一皮袋酒，并一只山羊羔，都驮在驴上，交给他儿子大卫，送与扫罗。
+        21 大卫到了扫罗那里，就侍立在扫罗面前。扫罗甚喜爱他，他就作了扫罗拿兵器的人。
+        22 扫罗差遣人去见耶西，说，求你容大卫侍立在我面前，因为他在我眼前蒙了恩。
+        23 从神那里来的恶魔临到扫罗身上的时候，大卫就拿琴，用手而弹，扫罗便舒畅爽快，恶魔离了他。
+        撒母耳上16:14-23–1Samuel16:14-23
   `,
-    author: "Wei Di",
+    author: "Tang han",
     date: "September 29, 2023",
-    img: "/assets/home/487147_3fb21a460ea840f08b896c1b6839e9e8mv2.png",
+    img: "/assets/blog/piano_gospel.PNG",
   },
 
   {
@@ -89,7 +92,7 @@ const blogs = [
 
     路得记讲述了一个关于忠诚、家庭的感人故事，它启示出了神对人的惊人恩典和祝福。
     主要人物和启示：
-    1. 路德：路德是一个摩押女子。她的出身并不是神祝福的，但是路德依靠自己忠诚的品质、对神的认可，以及身体力行让路德成为最终成长为神自己家里的人。她启示出，我们每一位对神有认可的人无论出身都可以成长为神家里的人并得神的恩典。
+    1. 路德：路德是一个摩押女子。她的出身并不是神祝福的，但是依靠自己忠诚的品质、对神的认可，以及身体力行让路德成为最终成长为神自己家里的人。她启示出，我们每一位对神有认可的人无论出身都可以成长为神家里的人并得神的恩典。
     2. 拿俄米：路德的婆婆。虽然失去丈夫和孩子，但是她身上体现出的品质吸引路德，她是真正关于路德的人，帮助路德一路不断成长。她启示出了，每个人都需要身边有这样的人关心，在爱和光中成长才能和神更亲近。
     3. 波阿斯：伯利恒当地的大财主。他身上善良、慷慨、救赎的品质启示出我们的主基督是何等的爱我们；他和路德的婚姻启示出，主才是真正爱我们、要我们的人，会去主动争取我们的利益。
 `,
