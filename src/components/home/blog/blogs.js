2db59914b83b82881f1e20402f9854841309f6ea
Yingson Yu <yingsonyu@gmail.com>
2023-10-05 10:22:50 -0500

    .

--- src/components/home/blog/blogs.js
+++ src/components/home/blog/blogs.js
@@ -53,25 +53,6 @@ const blogs = [
   //   img: "/assets/home/487147_3fb21a460ea840f08b896c1b6839e9e8mv2.png",
   // },
 
-<<<<<<< HEAD
-  // {
-  //   id: 4,
-  //   title: "Pumpkin Patch Meetup @7",
-  //   body: `RANDOM TEXT
-
-  //     Material confined likewise it humanity raillery an unpacked as he. Three chief merit no if. Now how her edward engage not horses. Oh resolution he dissimilar precaution to comparison an. Matters engaged between he of pursuit manners we moments. Merit gay end sight front. Manor equal it on again ye folly by match. In so melancholy as an sentiments simplicity connection. Far supply depart branch agreed old get our.
-
-  // Him boisterous invitation dispatched had connection inhabiting projection. By mutual an mr danger garret edward an. Diverted as strictly exertion addition no disposal by stanhill. This call wife do so sigh no gate felt. You and abode spite order get. Procuring far belonging our ourselves and certainly own perpetual continual. It elsewhere of sometimes or my certainty. Lain no as five or at high. Everything travelling set how law literature.
-
-  // Folly was these three and songs arose whose. Of in vicinity contempt together in possible branched. Assured company hastily looking garrets in oh. Most have love my gone to this so. Discovered interested prosperous the our affronting insipidity day. Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible.
-
-  // Much did had call new drew that kept. Limits expect wonder law she. Now has you views woman noisy match money rooms. To up remark it eldest length oh passed. Off because yet mistake feeling has men. Consulted disposing to moonlight ye extremity. Engage piqued in on coming.
-  // `,
-  //   author: "Wei Di",
-  //   date: "September 29, 2023",
-  //   img: "/assets/home/487147_3fb21a460ea840f08b896c1b6839e9e8mv2.png",
-  // },
-=======
   {
     id: 4,
     title: "Music Literacy Class",
@@ -92,7 +73,6 @@ const blogs = [
     date: "September 29, 2023",
     img: "/assets/blog/piano_gospel.PNG",
   },
->>>>>>> 38866f7c85259dddee3f68525df82b44a6ea73e6
 
   {
     id: 6,
